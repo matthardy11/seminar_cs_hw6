b3072a25ee850bd61e6dc92c38e06b4ef2985613
matthardy11 <matthardy2560@gmail.com>
2023-02-05 17:46:24 +0400

    complete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int FindPositive(int[] array)
+{
+  int result = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0)
+    {
+      result++;
+    }
+  }
+  return result;
+}
+
+int InputNumber(string str)
+{
+  int number;
+  string text;
+  while(true)
+  {
+    Console.WriteLine(str);
+    text = Console.ReadLine();
+    if(int.TryParse(text, out number))
+    {
+      break;
+    }
+    System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз.");
+  }
+  return number;
+}
+
+int[] FillArray(int size)
+{
+  int[] array = new int[size];
+  for(int i = 0; i < array.Length; i++)
+  {
+    array[i] = InputNumber("Введите число: ");
+  }
+  return array;
+}
+
+int size = InputNumber("Введите желаемый размер массива: ");
+System.Console.WriteLine("");
+System.Console.WriteLine("Колличество положительных чисел: " + FindPositive(FillArray(size)));
+
